b7a060a07960a0bc81f25a04dbbe5f4cc4f5563f
wahidullahRamaki <30998117+wahidullahRamaki@users.noreply.github.com>
2024-01-06 10:38:05 +0430

    rendering list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -10,7 +15,7 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1> 🏝 For Away 💼</h1>;
+  return <h1> 🌴 For Away 💼</h1>;
 }
 function Form() {
   return (
@@ -21,7 +26,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
